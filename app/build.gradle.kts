afe7330fd17e51f51d3f2411ba276d0bf29fb52c
Michał Małeczek <michalmaleczek@gmail.com>
2024-04-29 16:42:28 +0200

    Upgrade versions od implementations and add message from author to projekt.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,13 +51,13 @@ android {
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(platform("androidx.compose:compose-bom:2024.04.01"))
+    implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("com.google.android.material:material:1.11.0")
--- app/src/main/java/com/example/basicpsychea/ui/screens/Projekt.kt
+++ app/src/main/java/com/example/basicpsychea/ui/screens/Projekt.kt
@@ -3,28 +3,42 @@ package com.example.basicpsychea.ui.screens
 import android.content.Intent
 import android.net.Uri
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,6 +46,8 @@ import com.example.basicpsychea.R
 
 @Composable
 fun projekt(modifier: Modifier = Modifier) {
+    var showDialog by remember { mutableStateOf(false) }
+
     LazyColumn(
         modifier = modifier,
         verticalArrangement = Arrangement.SpaceBetween
@@ -71,6 +87,68 @@ fun projekt(modifier: Modifier = Modifier) {
                             modifier = Modifier.align(Alignment.CenterHorizontally)
                 )
                 Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_small)))
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp) // Odległość od krawędzi ekranu
+                        .clip(RoundedCornerShape(8.dp)), // Zaokrąglenie rogów
+
+                contentAlignment = Alignment.Center
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(8.dp)) // Zaokrąglenie rogów boxa
+                            .clickable { showDialog = true }, // Obsługa kliknięcia
+                        contentAlignment = Alignment.Center,
+                        content = {
+                            Box(
+                                modifier = Modifier
+                                    .width(180.dp) // Rozmiar tła
+                                    .height(110.dp)
+                                    .clip(RoundedCornerShape(8.dp)) // Zaokrąglenie rogów tła
+                                    .background(Color.White), // Kolor tła
+                                contentAlignment = Alignment.Center,
+                                content = {
+                                    Column(
+                                        horizontalAlignment = Alignment.CenterHorizontally,
+                                    ) {
+                                        Image(
+                                            painter = painterResource(id = R.drawable.baseline_mail_outline_24),
+                                            contentDescription = null,
+                                            contentScale = ContentScale.Fit,
+                                            modifier = Modifier
+                                                .size(50.dp), // Wielkość obrazka
+                                        )
+                                        Text(text = stringResource(R.string.wiadomo_od_tw_rc_w),
+                                            textAlign = TextAlign.Center,
+                                            fontFamily = FontFamily.Cursive)
+                                    }
+                                }
+                            )
+                        }
+                    )
+                }
+
+                if (showDialog) {
+                    AlertDialog(
+                        onDismissRequest = { showDialog = false },
+                        title = { Text(stringResource(R.string.zako_czenie_projektu)) },
+                        text = {
+                            Column {
+                                Text("Cześć! Nasz projekt dobiega końca. Dla naszego zespołu był to naprawdę wspaniały czas, liczę, że dla ciebie również. Zarówno my jak i wy nauczyliśmy się sporo podczas tego projektu. Zakończenie projektu nie oznacza oczywiście końca Psychea! Aplikacja będzie ciągle dostępna w Google Play i będzie przez Nas obsługiwana. Zachęcamy do dalszego korzystanie z naszej aplikacji i polecania jej znajomym i rodzinie. Jeśli masz jakieś pomysły odnośnie rozwoju aplikacji koniecznie daj nam o tym znać, a jeśli lubisz naszą aplikacje wystaw pozytywną ocenę w Google Play! Zachęcamy też do obserwowania naszych mediów społecznościowych.")
+                                Text(text = "Dzięki, że byliście z nami!")
+                                Text(text = stringResource(R.string.for_better_psyche))
+                                Spacer(modifier = Modifier.height(10.dp))
+                                Text(text = stringResource(R.string.kierownik_projektu_micha_ma_eczek))
+                            }
+                        },
+                        confirmButton = {
+                            TextButton(onClick = { showDialog = false }) {
+                                Text(text = stringResource(id = R.string.ok))
+                            }
+                        }
+                    )
+                }
                 Text(
                     text = stringResource(R.string.opis),
                     style = MaterialTheme.typography.headlineLarge,
